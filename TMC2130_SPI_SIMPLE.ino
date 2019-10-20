1b8f06c55aca6626e7a4f9780fe95b4757975f5f
margaret-n-m <51097001+margaret-n-m@users.noreply.github.com>
2019-10-20 15:29:16 +0200

    Update TMC2130_SPI_SIMPLE.ino

--- TMC2130_SPI_SIMPLE.ino
+++ TMC2130_SPI_SIMPLE.ino
@@ -4,9 +4,10 @@
 #define STEP 5
 #define EN 7
 #define CS 10
-#define MISO 1 //icsp header
-#define MOSI 4 //icsp header
-#define CLK 3 //icsp header
+#define MISO 1           //icsp header
+#define MOSI 4           //icsp header
+#define CLK 3            //icsp header
+
 
 //setting parameters
 
@@ -14,7 +15,7 @@ void setRegister(uint8_t adress, uint32_t newBytes) {
   uint8_t rx_buf[5];   
    
    SPI.beginTransaction(SPISettings(16000000/8, MSBFIRST, SPI_MODE3));
-   digitalWrite(CS, LOW); //chip select      
+   digitalWrite(CS, LOW);     //chip select enabled  
      rx_buf[0] = SPI.transfer(adress); 
      rx_buf[1] = SPI.transfer((newBytes & 0xFF000000) >> 24); 
      rx_buf[2] = SPI.transfer((newBytes & 0xFF0000) >> 16); 
@@ -23,7 +24,7 @@ void setRegister(uint8_t adress, uint32_t newBytes) {
    digitalWrite(CS, HIGH);
    SPI.endTransaction();   
    
-//checking out what was sent via SPI  
+//CHECKING OUT WHAT WAS SENT VIA SPI
        Serial.print("Sent:  ");        
        Serial.print(adress, HEX);
        Serial.print(((newBytes & 0xFF000000) >> 24), HEX);
@@ -31,7 +32,7 @@ void setRegister(uint8_t adress, uint32_t newBytes) {
        Serial.print(((newBytes & 0xFF00) >> 8), HEX);
        Serial.println((newBytes & 0xFF), HEX);  
        
-//checking out what came back via SPI
+//CHECKING OUT WHAT WAS RECEIVED VIA SPI
        Serial.print("Received:  ");  
       for(int i = 0; i<5; i++){
        Serial.print(rx_buf[i], HEX);
@@ -46,37 +47,84 @@ void setup() {
   pinMode(EN, OUTPUT);
   pinMode(CS, OUTPUT);
 
-  digitalWrite(EN, LOW); //switching the driver on
-  digitalWrite(CS, HIGH);
+  digitalWrite(EN, LOW);        //switching the driver on
+  digitalWrite(CS, HIGH);       //chip select disabled
 
-//setting the parameters according to the datasheet
+//SETTING THE PARAMETERS
     Serial.begin(9600);
     SPI.begin();
+
       delay(100);
-      setRegister(0xEC, 0x000080C8); //vsense 1, TOFF 8, TBL 1, HSTRT 4, HEND 1, CHM 0 - spreadCycle
+      setRegister(0x80, 0x00000000); // GCONF: no analog scaling, stealthChop disabled
       delay(100);
-      setRegister(0x90, 0x0001130A); //I_RUN  19 I_HOLD 10 (calculated, normally 70% of I_RUN), IHOLDDELAY 1
+      setRegister(0x90, 0x00000000); // IHOLD IRUN: all current 0 (I_RUN 0, I_HOLD 0, IHOLDDELAY 0)
       delay(100);
-      setRegister(0x91, 0x0000000A); //TPOWERDOWN 10
+      setRegister(0x91, 0x00000000); // TPOWERDOWN: 0, immediate power down
       delay(100);
-      setRegister(0x80, 0x00000004); // no analog scaling, en_pwm_mode 1
+      setRegister(0x93, 0x00000000); // TPWMTHRS: upper velocity for stealthChop disabled
       delay(100);
-      setRegister(0x93, 0x000001F4); //stealthChop (TPWMTHRS)
+      setRegister(0x94, 0x00000000); // TCOOLTHRS: lower velocity for CoolStep disabled
       delay(100);
-      setRegister(0xF0, 0x000401C8); //autoPWM, 2/1024 Fclk
+      setRegister(0x95, 0x00000000); // THIGH: velocity dependent swithing to another mode disabled
       delay(100);
+      setRegister(0xAD, 0x00000000); // XDIRECT: normal SPI operation
+      delay(100);
+      setRegister(0xB3, 0x00000000); // VDCMIN: automatic commutation dcStep disabled
+      delay(100);
+      setRegister(0xEC, 0x00000000); // CHOPCONF: native 256 microstepping, vsense 0, TBL 0, chm 0, HEND 0, HSTRT 0, TOFF 0
+      delay(100);
+/*    setRegister(0xE0, 0xAAAAB554); // MSLUT0
+      delay(100);
+      setRegister(0xE1, 0x4A9554AA); // MSLUT1
+      delay(100);
+      setRegister(0xE2, 0x24492929); // MSLUT2
+      delay(100);
+      setRegister(0xE3, 0x10104222); // MSLUT3
+      delay(100);
+      setRegister(0xE4, 0xFBFFFFFF); // MSLUT4
+      delay(100);
+      setRegister(0xE5, 0xB5BB777D); // MSLUT5
+      delay(100);
+      setRegister(0xE6, 0x49295556); // MSLUT6
+      delay(100);   
+      setRegister(0xE7, 0x00404222); // MSLUT7
+      delay(100); 
+      setRegister(0xE8, 0xFFFF8056); // MSLUTSEL
+      delay(100);
+      setRegister(0xE0, 0xAAAAB554); // MSLUT0
+      delay(100);
+      setRegister(0xE0, 0xAAAAB554); // MSLUT0
+      delay(100);
+      setRegister(0xE9, 0x00F70000); // MSLUTSTART
+      delay(100); */
+      setRegister(0xED, 0x00000000); // COOLCONF: standard stallGuard mode, threshold value 0, seimin 1/2 I_RUN, semax 0, semin 0 (coolStep off)
+      delay(100); 
+      setRegister(0xF0, 0x00050480); // PWMCONF: normal standstill operation, standard PWM mode, automatic current control, fPWM = 2/683fCLK, PWM_GRAD 4, PWM_AMPL 128
+      delay(100); 
+      setRegister(0xF2, 0x00000000); // ENCM_CTRL: encoder mode configuration disabled
+      delay(100);           
+/*    setRegister(0xEE, 0x00000000); // DCCTRL
+      delay(100);    
+      setRegister(0x01, 0x00000000); // GSTAT: read only
+      delay(100);  */
+      setRegister(0xEC, 0x00024008); // CHOPCONF: native 256 microstepping, vsense 1, TBL 1, chm 0, HEND 0, HSTRT 0, TOFF 8
+      delay(100);     
+      setRegister(0x90, 0x00001309); // IHOLD_IRUN: I_RUN  19 I_HOLD 9 (calculated, normally 70% of I_RUN), IHOLDDELAY 0
+      delay(100);                      
       setRegister(0x6F, 0x00000000); //reading the DRV_STATUS
       delay(100);
       setRegister(0x6F, 0x00000000); //additional transfer to receive last inquiery
       delay(100);
+      
     SPI.end();
-  digitalWrite(DIR, HIGH); //direction setting
+   
+  digitalWrite(DIR, LOW);       //direction setting
 }
 
 void loop() {
-  
-  digitalWrite(STEP, HIGH); //lets make some steps
-  delay(1000);
+  digitalWrite(STEP, HIGH);
+  delayMicroseconds(10);
   digitalWrite(STEP, LOW);
-  delay(1000);
+  delayMicroseconds(10);
+
 }
